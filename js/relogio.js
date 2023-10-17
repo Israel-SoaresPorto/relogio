98cf74bce4e8dbf7a839b5e27d3a0b6dd287b039
Israel-SoaresPorto <isaelporto15@gmail.com>
2023-10-17 10:25:38 -0300

    alteracao de cor de fundo

--- js/relogio.js
+++ js/relogio.js
@@ -6,6 +6,9 @@ const dia = document.querySelector("#dia")
 const mes = document.querySelector("#mes")
 const ano = document.querySelector("#ano")
 const diaDaSemana = document.querySelector("#diaSemana")
+const periodo = document.querySelector("#periodo")
+const icone = document.querySelector("#icone")
+const fundo = document.body
 
 let data = new Date()
 let diasDasSemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"]
@@ -31,6 +34,20 @@ function relogio () {
     let hr = horario.getHours()
     let min = horario.getMinutes()
     let seg = horario.getSeconds()
+
+    if(hr >= 5 && hr < 12) {
+        periodo.textContent = "Bom dia!"
+        icone.src = "images/sun.png"
+        fundo.classList.add("fundo-dia")
+    } else if(hr >= 12 && hr < 18) {
+        periodo.textContent = "Boa tarde!"
+        icone.src = "images/sun.png"
+        fundo.classList.add("fundo-tarde")
+    } else {
+        periodo.textContent = "Boa noite!"
+        icone.src = "images/moon.png"
+        fundo.classList.add("fundo-noite")
+    }
 
     if(hr < 10) {
         hr = '0' + hr
